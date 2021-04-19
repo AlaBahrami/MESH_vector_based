36e9c880f4ee0b87e094a59b075238a15f5f6d1d
Ala <66693414+AlaBahrami@users.noreply.github.com>
2021-04-18 19:32:53 -0600

    Update summa_forcing.py

--- Code/forcing/summa_forcing.py
+++ Code/forcing/summa_forcing.py
@@ -10,7 +10,7 @@ PROGRAMMER(S)
     Ala Bahrami
 REVISION HISTORY
     20210414 -- Initial version created and posted online
-    20210414 -- 
+    20210418 -- adding different forcing variables  
 REFERENCES
 
 """
@@ -38,59 +38,138 @@ new_rank, drainage_db = new_rank_extract(input_ddb)
 sum_forc = xs.open_dataset(input_forc, drop_variables=['data_step', 'hruId'])
 # sum_forc = xs.open_dataset(input_forc)
 sum_forc.close()
-print(sum_forc.variables.keys())
+# print(sum_forc.variables.keys())
 
 #%% reorder input forcing based on new_rank
 for m in ['lat','lon']:
     sum_forc[m].values = sum_forc[m].values[new_rank]
 for n in ['pptrate', 'SWRadAtm','LWRadAtm','airtemp','windspd','airpres','spechum']:
+   
     if (sum_forc[n].dims[0] == 'time'):
         sum_forc[n].values = sum_forc[n].values[: , new_rank]
     else:
         sum_forc[n].values = sum_forc[n].values[new_rank , :]
 
-for n in ['pptrate', 'SWRadAtm','LWRadAtm','airtemp','windspd','airpres','spechum']:
-
+for n in ['pptrate', 'SWRadAtm','LWRadAtm','airtemp','windspd','airpres','spechum']:   
     # Transpose 'averageRoutedRunoff' to match the desired order of dimensions.
     if (sum_forc[n].dims[0] == 'time'):
         v = sum_forc[n].values.transpose()
     else:
         v = sum_forc[n].values
+
+    # Define the xarray
+    if (n == 'pptrate'):
+            forc = xs.DataArray(v, 
+                 coords = dict(time = (['time'], 
+                 sum_forc['time'].values.copy()), lat = (['subbasin'], 
+                 drainage_db['lat'].values.copy()), lon = (['subbasin'], 
+                 drainage_db['lon'].values.copy())), 
+                 dims = ['subbasin', 'time'])
+              
+            # Update the forcing attributes (including the updated units).
+            forc.attrs.update(units = sum_forc[n].units, grid_mapping = 'crs')
+             
+            # Override the encoding of forcing variable to specify 'time' in the list of 'coordinates' (omitted if done automatically).
+            forc.encoding['coordinates'] = 'time lon lat'
     
-    # Define the xarray.
-    forc = xs.DataArray(v, coords = dict(time = (['time'], 
-         sum_forc['time'].values.copy()), lat = (['subbasin'], 
-         drainage_db['lat'].values.copy()), lon = (['subbasin'], 
-         drainage_db['lon'].values.copy())), dims = ['subbasin', 'time'])
-      
-    # Update the forcing attributes (including the updated units).
-    forc.attrs.update(units = sum_forc[n].units, grid_mapping = 'crs')
-     
-    # Override the encoding of forcing variable to specify 'time' in the list of 'coordinates' (omitted if done automatically).
-    forc.encoding['coordinates'] = 'time lon lat'
-     
-    # Create a new data file using fields from 'drainage_db'.
-    data_out = xs.Dataset({n: forc}, attrs = dict(Conventions = 'CF-1.6', featureType = 'timeSeries'))
-     
-    # Update attributes.
-    data_out['time'].attrs.update(standard_name = 'time', axis = 'T')
-    data_out['lat'].attrs.update(standard_name = 'latitude', units = 'degrees_north', axis = 'Y')
-    data_out['lon'].attrs.update(standard_name = 'longitude', units = 'degrees_east', axis = 'X')
-     
-    # Override the encoding of 'time' to specify a 'gregorian' calendar (MESH might not recognize 'proleptic_gregorian').
-    data_out['time'].encoding['calendar'] = 'gregorian'
-     
-    # Define a variable for the points and set the 'timeseries_id' (required for some viewers).
-    data_out['subbasin'] = (['subbasin'], drainage_db['seg_id'].values.astype(np.int32).astype('S20'))
-    data_out['subbasin'].attrs['cf_role'] = 'timeseries_id'
-     
-    # Copy 'crs' from 'drainage_db'.
-    data_out['crs'] = drainage_db['crs'].copy()
-     
-    # Override the file 'encoding' to define 'time' as an 'UNLIMITED' dimension.
-    data_out.encoding.update(unlimited_dims = 'time')
+    elif(n == 'SWRadAtm'):
+            forc1 = xs.DataArray(v, coords = dict(time = (['time'], 
+                 sum_forc['time'].values.copy()), lat = (['subbasin'], 
+                 drainage_db['lat'].values.copy()), lon = (['subbasin'], 
+                 drainage_db['lon'].values.copy())), dims = ['subbasin', 'time'])
+              
+            # Update the forcing attributes (including the updated units).
+            forc1.attrs.update(units = sum_forc[n].units, grid_mapping = 'crs')
+             
+            # Override the encoding of forcing variable to specify 'time' in the list of 'coordinates' (omitted if done automatically).
+            forc1.encoding['coordinates'] = 'time lon lat'
+            
+    elif(n == 'LWRadAtm'):
+            forc2 = xs.DataArray(v, coords = dict(time = (['time'], 
+                 sum_forc['time'].values.copy()), lat = (['subbasin'], 
+                 drainage_db['lat'].values.copy()), lon = (['subbasin'], 
+                 drainage_db['lon'].values.copy())), dims = ['subbasin', 'time'])
+              
+            # Update the forcing attributes (including the updated units).
+            forc2.attrs.update(units = sum_forc[n].units, grid_mapping = 'crs')
+             
+            # Override the encoding of forcing variable to specify 'time' in the list of 'coordinates' (omitted if done automatically).
+            forc2.encoding['coordinates'] = 'time lon lat'        
+                    
+    elif(n == 'airtemp'):
+            forc3 = xs.DataArray(v, coords = dict(time = (['time'], 
+                 sum_forc['time'].values.copy()), lat = (['subbasin'], 
+                 drainage_db['lat'].values.copy()), lon = (['subbasin'], 
+                 drainage_db['lon'].values.copy())), dims = ['subbasin', 'time'])
+              
+            # Update the forcing attributes (including the updated units).
+            forc3.attrs.update(units = sum_forc[n].units, grid_mapping = 'crs')
+             
+            # Override the encoding of forcing variable to specify 'time' in the list of 'coordinates' (omitted if done automatically).
+            forc3.encoding['coordinates'] = 'time lon lat'  
+            
+    elif(n == 'windspd'):
+            forc4 = xs.DataArray(v, coords = dict(time = (['time'], 
+                 sum_forc['time'].values.copy()), lat = (['subbasin'], 
+                 drainage_db['lat'].values.copy()), lon = (['subbasin'], 
+                 drainage_db['lon'].values.copy())), dims = ['subbasin', 'time'])
+              
+            # Update the forcing attributes (including the updated units).
+            forc4.attrs.update(units = sum_forc[n].units, grid_mapping = 'crs')
+             
+            # Override the encoding of forcing variable to specify 'time' in the list of 'coordinates' (omitted if done automatically).
+            forc4.encoding['coordinates'] = 'time lon lat' 
+            
+    elif(n == 'airpres'):
+            forc5 = xs.DataArray(v, coords = dict(time = (['time'], 
+                 sum_forc['time'].values.copy()), lat = (['subbasin'], 
+                 drainage_db['lat'].values.copy()), lon = (['subbasin'], 
+                 drainage_db['lon'].values.copy())), dims = ['subbasin', 'time'])
+              
+            # Update the forcing attributes (including the updated units).
+            forc5.attrs.update(units = sum_forc[n].units, grid_mapping = 'crs')
+             
+            # Override the encoding of forcing variable to specify 'time' in the list of 'coordinates' (omitted if done automatically).
+            forc5.encoding['coordinates'] = 'time lon lat'
+            
+    else:
+            forc6 = xs.DataArray(v, coords = dict(time = (['time'], 
+                 sum_forc['time'].values.copy()), lat = (['subbasin'], 
+                 drainage_db['lat'].values.copy()), lon = (['subbasin'], 
+                 drainage_db['lon'].values.copy())), dims = ['subbasin', 'time'])
+              
+            # Update the forcing attributes (including the updated units).
+            forc6.attrs.update(units = sum_forc[n].units, grid_mapping = 'crs')
+             
+            # Override the encoding of forcing variable to specify 'time' in the list of 'coordinates' (omitted if done automatically).
+            forc6.encoding['coordinates'] = 'time lon lat'        
+
+
+#%% Create a new data file using fields from 'drainage_db'.
+# !+ the forc array should be replaced by loop using concat
+data_out = xs.Dataset({'pptrate': forc, 'SWRadAtm' : forc1, 'LWRadAtm' : forc2,
+                       'airtemp': forc3, 'windspd': forc4, 'airpres':forc5, 'spechum': forc6}, 
+                      attrs = dict(Conventions = 'CF-1.6', featureType = 'timeSeries'))
+ 
+# Update attributes.
+data_out['time'].attrs.update(standard_name = 'time', axis = 'T')
+data_out['lat'].attrs.update(standard_name = 'latitude', units = 'degrees_north', axis = 'Y')
+data_out['lon'].attrs.update(standard_name = 'longitude', units = 'degrees_east', axis = 'X')
+ 
+# Override the encoding of 'time' to specify a 'gregorian' calendar (MESH might not recognize 'proleptic_gregorian').
+data_out['time'].encoding['calendar'] = 'gregorian'
+ 
+# Define a variable for the points and set the 'timeseries_id' (required for some viewers).
+data_out['subbasin'] = (['subbasin'], drainage_db['seg_id'].values.astype(np.int32).astype('S20'))
+data_out['subbasin'].attrs['cf_role'] = 'timeseries_id'
+ 
+# Copy 'crs' from 'drainage_db'.
+data_out['crs'] = drainage_db['crs'].copy()
+ 
+# Override the file 'encoding' to define 'time' as an 'UNLIMITED' dimension.
+data_out.encoding.update(unlimited_dims = 'time')
 
 # %% save netcdf to an output 
-#sum_forc.to_netcdf(output_forc)
+sum_forc.to_netcdf(output_forc)
 
 
